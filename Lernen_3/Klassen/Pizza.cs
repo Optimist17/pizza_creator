cdec7a4fc054ad7716527aea2182965276822d4d
Martin Guelland <martin.guelland@googlemail.com>
2021-01-25 19:27:05 +0100

    Projektdateien hinzufügen.

---
+++ Lernen_3/Klassen/Pizza.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lernen_3.Klassen
+{
+    public class Pizza
+    {
+
+        #region Attributes
+
+        public Double Diameter { get; private set; }
+        public Double Range { get; private set; }
+        public String Name { get; private set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public void ChangeSize(Double diameter)
+        {
+            Diameter = diameter;
+            Range = diameter * Math.PI;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Pizza()
+        {
+            Diameter = 2;
+        }
+
+        public Pizza(String name, Double diameter)
+        {
+            ChangeSize(diameter);
+            Name = name;
+        }
+        
+        #endregion
+
+
+    }
+}
---
+++ Lernen_3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Lernen_3.Klassen;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lernen_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Double diameter = Double.Parse(PizzaDiameter.Text);
+            Pizza pizza = new Pizza(PizzaName.Text, diameter);
+            Anzeige.Content = pizza.Name + " hat einen Durchmesser von " + pizza.Diameter + "cm und einen Umfang von " + Math.Round(pizza.Range, 2) + "cm.";
+        }
+    }
+}
